b41f93fe232306fcabc22813d50e77cd10e60534
Marco1111998 <96786575+Marco1111998@users.noreply.github.com>
2026-06-19 11:22:02 +0100

    Update streams.sh

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -16,7 +16,7 @@ sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internaci
 
 # TVI Reality - update the stream URL of TVI Reality
 
-sed -i "/live_tvi_direct/ c https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+sed -i "/live_tvi_reality/ c https://video-auth6.iol.pt/live_tvi_reality/live_tvi_reality/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
 
 # Porto Canal - update the stream URL of Porto Canal
 
